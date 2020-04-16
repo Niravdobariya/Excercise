3ceb05c2dec483a9808113ed626af79021d188c8
Niravdobariya <dbnirav007@gmail.com>
2020-04-16 16:04:31 +0530

    Update Grep.java

--- src/Grep.java
+++ src/Grep.java
@@ -1,20 +1,26 @@
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
-public class Grep {
+public class Grep7 {
 
     static BufferedWriter bw;
+    static AtomicLong timeIO;
+    static AtomicLong searchOp;
+
     public static void main(String args[]) throws Exception {
 
+        timeIO = new AtomicLong();
+        searchOp = new AtomicLong();
         String pattern = args[0];
         long time = System.currentTimeMillis();
 
         bw = new BufferedWriter(new FileWriter("/Users/niravdobariya/Desktop/output.txt"));
-        ExecutorService poolExecutor = Executors.newFixedThreadPool(2);
+        ExecutorService poolExecutor = new MyThreadPoolExecutor(16);
         for (int i = 1; i < args.length; i++) {
             try {
                 search(poolExecutor, args[i], pattern);
@@ -24,6 +30,8 @@ public class Grep {
         }
         poolExecutor.shutdown();
         poolExecutor.awaitTermination(10L, TimeUnit.MINUTES);
+        System.out.println("Time IO : " + timeIO.get());
+        System.out.println("Search IO : " + searchOp.get());
         System.out.println(System.currentTimeMillis() - time);
         bw.flush();
         bw.close();
@@ -53,91 +61,36 @@ public class Grep {
                     if (fileSearch(dir, pattern)) {
                         //                    System.out.println(dir.getName() + " contains  " + pattern.p);
                     }
-                } catch (IOException | InterruptedException | ExecutionException e) {
-                    e.printStackTrace();
-                    System.out.println(dir.getPath() + " : Exception caught during reading File");
+                } catch (IOException e) {
+                    System.out.println(dir.getName() + " : Exception caught during reading File");
                 }
             });
 
         }
     }
-
-    private static boolean fileSearch(File file, Pattern p) throws IOException, InterruptedException, ExecutionException {
-        int poolSize = 5;
-        ExecutorService poolExecutor = Executors.newFixedThreadPool(poolSize);
-        long chunkSize = 8192;
-        long cnt = (file.length() + chunkSize - 1) / chunkSize;
-        int taskAtTime = 10240;
-        int matchedCount = 0;
-        long start = 0;
-        long end = Long.min(chunkSize, file.length());
-        List<Callable<char[]>> tasks = new ArrayList<>();
+    private static boolean fileSearch(File file, Pattern p) throws IOException {
+        RandomAccessFile aFile = new RandomAccessFile(file.getPath(), "r");
+        FileChannel inChannel = aFile.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        char[] text = new char[1024];
+        int cnt = 0;
+        boolean flag = true;
         Matcher m = p.matcher();
-        boolean flag = false;
-
-        while (cnt > 0) {
-            int bound = (int) Long.min(cnt, taskAtTime);
-
-            List<Future<char[]>> futures = new ArrayList<>();
-            for (int i = 0; i < bound; i++) {
-                futures.add(poolExecutor.submit(createTask(file, start, end)));
-                start = end;
-                end = Long.min(end + chunkSize, file.length());
+        while(inChannel.read(buffer) > 0)
+        {
+            buffer.flip();
+            for (int i = 0; i < buffer.limit(); i++)
+            {
+                text[i] = (char) buffer.get();
             }
-            for (Future<char[]> future : futures) {
-                char[] s = future.get();
-                if (s == null) {
-                    continue;
-                }
-                matchedCount += m.search(s);
-            }
-            cnt -= bound;
+            cnt += m.search(text);
+            buffer.clear();
         }
-        System.out.println(file.getPath() + " : " + matchedCount);
-        poolExecutor.shutdown();
-        if(matchedCount > 0) flag = true;
+        System.out.println(file.getPath() + " : " + cnt);
+        if(cnt > 0)flag =true;
+        inChannel.close();
+        aFile.close();
         return flag;
     }
 
-    private static char[] processPart(File file, long start, long end) throws IOException {
-
-        RandomAccessFile rFile = null;
-        FileChannel fc = null;
-        try {
-
-            rFile = new RandomAccessFile(file.getPath(),"r");
-            rFile.seek(start);
-            fc = rFile.getChannel();
-            ByteBuffer bb = ByteBuffer.allocate(8192);
-            if(start > end) return null;
-            int size = (int) (end - start);
-            int i = 0;
-
-            if (fc.read(bb) != -1) {
-
-                bb.flip();
-                char[] ret = new char[size];
-                while(bb.hasRemaining()) {
-                    ret[i++] = (char) bb.get();
-                }
-                bb.clear();
-                return ret;
-            }
-            return null;
-        } finally {
-            if(fc != null)
-                fc.close();
-            if(rFile != null)
-                rFile.close();
-        }
-    }
-
-    private static Callable<char[]> createTask(File file, long start, long end) {
-        return new Callable<char[]>() {
-            @Override
-            public char[] call() throws Exception {
-                return processPart(file, start, end);
-            }
-        };
-    }
 }
